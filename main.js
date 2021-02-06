311fac26a8e3505680f8296d4e3c47692ca38872
Hasan Siam <hasansiam4545@gmail.com>
2021-02-07 02:32:30 +0600

    for each loop added

--- main.js
+++ main.js
@@ -5,16 +5,32 @@ document.getElementById('btn').addEventListener('click', function () {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
         .then(res => res.json())
         .then(data => {
-            const meal = document.getElementById('meal');
-            const mealImg = data.meals[0].strMealThumb;
-            const mealName = data.meals[0].strMeal;
-            const mealDiv = document.createElement('div');
-            const mealInfo = `
+
+            const meals = data.meals;
+            meals.forEach(meal => {
+                const mealList = document.getElementById('mealList');
+                const mealDiv = document.createElement('div');
+                const mealName = meal.strMeal;
+                const mealImg = meal.strMealThumb;
+                  const mealInfo = `
             <img src = "${mealImg}">
             <h2>${mealName}</h2>
             `
-            mealDiv.innerHTML = mealInfo;
-            meal.appendChild(mealDiv);
+                mealDiv.innerHTML = mealInfo;
+                mealList.appendChild(mealDiv);
+            });
+
+          
+            // const meal = document.getElementById('meal');
+            // const mealImg = data.meals[0].strMealThumb;
+            // const mealName = data.meals[0].strMeal;
+            // const mealDiv = document.createElement('div');
+            // const mealInfo = `
+            // <img src = "${mealImg}">
+            // <h2>${mealName}</h2>
+            // `
+            // mealDiv.innerHTML = mealInfo;
+            // meal.appendChild(mealDiv);
 
        
             
