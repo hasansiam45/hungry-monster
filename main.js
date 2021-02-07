4dad834a99d35626f8bb3ba3f4df07dd424df0cc
Hasan Siam <hasansiam4545@gmail.com>
2021-02-07 13:05:51 +0600

    meal details added

--- main.js
+++ main.js
@@ -1,27 +1,66 @@
+let mealList = document.getElementById('mealList');
+let topInfo = document.getElementById('details');
+
+document.getElementById('btn').addEventListener('click', displayMatchingFoods);
+document.getElementById('input').addEventListener('change', displayMatchingFoods);
 
 
-document.getElementById('btn').addEventListener('click', function () {
-    const userInput = document.getElementById('input').value;
+
+
+function displayMatchingFoods() {
+    userInput = document.getElementById('input').value;
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`)
-        .then(res => res.json())
-        .then(data => {
 
+        .then(res => res.json())
+
+        .then(data => {
             const meals = data.meals;
             meals.forEach(meal => {
-                const mealList = document.getElementById('mealList');
+
                 const mealDiv = document.createElement('div');
                 const mealName = meal.strMeal;
                 const mealImg = meal.strMealThumb;
-                  const mealInfo = `
-            <img src = "${mealImg}">
-            <h2>${mealName}</h2>
+                const mealInfo = `
+            <img onclick = "displayDetails('${mealName}')" src = "${mealImg}">
+            <h2 onclick = "displayDetails('${mealName}')">${mealName}</h2>
             `
                 mealDiv.innerHTML = mealInfo;
                 mealList.appendChild(mealDiv);
             });
 
-            
         })
-    
-    
-})
+
+    mealList.innerHTML = null;
+}
+
+
+const displayDetails = name => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const idMeal = data.meals[0].idMeal;
+            fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+                .then(res => res.json())
+                .then(data => {
+                    const detailsInfo = document.createElement('div');
+                    const detailsOfMeal = data.meals[0];
+                    const ingredientInfo = `
+                    <img src ="${detailsOfMeal.strMealThumb}">
+                    <h1>${detailsOfMeal.strMeal}</h1>
+                    <h3>Ingredients: </h3>
+                    <h3>1. ${detailsOfMeal.strIngredient1}</h3>
+                    <h3>2. ${detailsOfMeal.strIngredient2}</h3>
+                    <h3>3. ${detailsOfMeal.strIngredient3}</h3>
+                    <h3>4. ${detailsOfMeal.strIngredient4}</h3>
+                    <h3>5. ${detailsOfMeal.strIngredient5}</h3>
+                    <h3>6. ${detailsOfMeal.strIngredient6}</h3>
+
+                    `
+                    detailsInfo.innerHTML = ingredientInfo;
+                    topInfo.appendChild(detailsInfo);
+
+                });
+        })
+    topInfo.innerHTML = null;
+}
